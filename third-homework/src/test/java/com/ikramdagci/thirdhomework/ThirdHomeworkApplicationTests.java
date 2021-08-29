005620f9b0f8905d495bceeec8a1f99b537f528e
İkram <ikramdgc@gmail.com>
2021-08-29 19:33:33 +0300

    module created

---
+++ third-homework/src/test/java/com/ikramdagci/thirdhomework/ThirdHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikramdagci.thirdhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
